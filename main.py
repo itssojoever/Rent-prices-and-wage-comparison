704a6c248226d1a7c67324c9a4e5062afba4de59
JoeEnterAUsername <144183809+JoeEnterAUsername@users.noreply.github.com>
2024-05-07 17:22:58 +0100

    Learning how XLwings works

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import xlwings as xl
+
+filepath = "priceindexofprivaterentsukhistoricalseriesaccessible.xlsx"
+
+wb1 = xl.Book(filepath)
+sheet1 = wb1.sheets["Table 3"]
+
+rentValues = sheet1.range("D5:D234").value
+monthValues = sheet1.range("A5:A234").value
+
+for value in rentValues:
+    print (f"£{value}")
 
 
-filepath = ""
+wb1.save(filepath)
+wb1.close()
+
